1807b37b0a88ad886f0a051f54cb903320b15b98
Connor Callahan <connorwcallahan@gmail.com>
2018-12-18 20:31:49 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+const shoeContainer = document.querySelector('#container')
+
+
+shoeContainer.addEventListener('click', (e) => {
+  console.log(e.target.className)
+})
+
+})
