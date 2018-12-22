b863f1939e75a2344d9a4a259b295a1f1cf91051
Connor Callahan <connorwcallahan@gmail.com>
2018-12-22 06:20:20 -0500

    started the js

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 document.addEventListener('DOMContentLoaded', () => {
 
 const shoeContainer = document.querySelector('#container')
+const colorID = document.querySelector('#color-ID')
+let shoePatch
 
-
+//grabbing the outer transparent patch
 shoeContainer.addEventListener('click', (e) => {
-  console.log(e.target.className)
+  if(e.target.tagName == 'path') {
+    shoePatch = e.target
+  }
+  innerShoePatch = shoePatch.dataset.id.slice(6, shoePatch.dataset.id.length)
+
+  const patchChange = document.getElementById(innerShoePatch)
+
+  patchChange.style.fill = 'lime'
 })
+
+colorID.addEventListener('click', (e) => {
+  console.log(e.target
+  )
+})
+
+// colorID.addEventListener('click', (e) => {
+//   console.log(e.target)
+// })
+
 
 })
